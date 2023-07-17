c134eaa605a9f90049c3f98f6d9b8a72f8adabbc
Manfred Scheucher <manfred.scheucher1@gmail.com>
2023-07-17 15:40:42 +0200

    .

--- enum_settings.py
+++ enum_settings.py
@@ -4,7 +4,8 @@ from more_itertools import powerset
 
 found = []
 
-for setting in powerset(['+-+-','+--+','+---','-+-+','-+--','--+-','---+','----']):
+potential = ['+-+-','+--+','+---','-+-+','-+--','--+-','---+','----']
+for setting in powerset(potential):
 	setting = list(sorted(setting))
 
 	lexmin = setting[:]
@@ -15,10 +16,10 @@ for setting in powerset(['+-+-','+--+','+---','-+-+','-+--','--+-','---+','----'
 				setting2 = [pattern[::-1] for pattern in setting2]
 			if negation:
 				setting2 = [''.join(('+' if c == '-' else '-') for c in pattern) for pattern in setting2]
-		
+			
 			#print("setting2",setting2)
 			setting2 = list(sorted(setting2))
-			if setting2 < lexmin:
+			if set(setting2).issubset(potential) and setting2 < lexmin:
 				lexmin = setting2[:]
 
 	if lexmin not in found:
