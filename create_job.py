383d500323e219137fd7a2c1214bd61fe95ab053
Manfred Scheucher <manfred.scheucher1@gmail.com>
2024-01-28 10:58:54 +0100

    .

--- create_job.py
+++ create_job.py
@@ -5,16 +5,17 @@ n = int(argv[2])
 
 params = path+" "+str(n)
 params_ = path+"_"+str(n)
-params_short = params_.split("/")[-1]  if "/" in params_  else params_
+path_short = path.split("/")[-1]  if "/" in params_  else params_
+name = path_short[-4:]+"_"+str(n)
 
-with open(params_+".job","w") as f: 
-    f.write("#!/bin/bash --login\n")
-    f.write(f"#SBATCH --job-name={params_short}\n")
-    f.write(f"#SBATCH --output={params_short}.out\n")
-    #f.write("#SBATCH --mem=30G\n")
-    f.write("#SBATCH --time=168:00:00\n")
-    #f.write("#SBATCH --cpus-per-task=32\n")
-    f.write("#SBATCH --mail-user=scheucher@math.tu-berlin.de\n")
-    f.write("#SBATCH --mail-type=BEGIN,END\n")
-    f.write("time python3 find_gadgets_new.py "+params+" --algorithm advanced\n")
-    f.write("time python3 find_gadgets_new.py "+params+" --algorithm basic\n")
+for algo in ['basic','advanced']:
+    with open(params_+".job_"+algo,"w") as f: 
+        f.write("#!/bin/bash --login\n")
+        f.write(f"#SBATCH --job-name={name}\n")
+        f.write(f"#SBATCH --output={name}.out\n")
+        #f.write("#SBATCH --mem=30G\n")
+        f.write("#SBATCH --time=12:00:00\n")
+        #f.write("#SBATCH --cpus-per-task=32\n")
+        f.write("#SBATCH --mail-user=scheucher@math.tu-berlin.de\n")
+        f.write("#SBATCH --mail-type=BEGIN,END\n")
+        f.write("time python3 find_gadgets_new.py "+params+" --timeout 3600 --algorithm "+algo+"\n")
